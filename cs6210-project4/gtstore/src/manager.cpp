8194f8826f5fbc77297f07e4907554217aa71e0d
GopeshSingal <gopesh.singal.03@gmail.com>
2024-12-03 14:26:49 -0500

    Tried to start the grpc implementation

--- cs6210-project4/gtstore/src/manager.cpp
+++ cs6210-project4/gtstore/src/manager.cpp
@@ -1,6 +1,84 @@
+#include "grpcpp/security/server_credentials.h"
+#include "grpcpp/server.h"
 #include "gtstore.hpp"
+// #include <functional>
+#include <grpcpp/server_context.h>
+#include <grpcpp/support/status_code_enum.h>
+#include <mutex>
+#include <unordered_map>
+#include <string>
+
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+#include "proto/gtstore.grpc.pb.h"
+#include "proto/gtstore.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+using gtstore::ManagerService;
+using gtstore::NodeInfo;
+using gtstore::RegisterAck;
+using gtstore::KeyRequest;
+using gtstore::NodeInfo;
 
 ConsistentHashing hashRing;
+
+class ManagerServiceImpl final : public ManagerService::Service {
+	private:
+		ConsistentHashing hash_ring;
+		mutex hr_mutex;
+	public:
+		ManagerServiceImpl(int virtual_nodes) {
+			hash_ring.init(virtual_nodes);
+		}
+		Status GetNodeForKey(ServerContext* context, KeyRequest* request, NodeInfo* response) {
+			lock_guard<mutex> lock(hr_mutex);
+			string node_id = hash_ring.getNode(request->key());
+			if (node_id.empty()) {
+				return Status(grpc::StatusCode::NOT_FOUND, "No nodes available");
+			}
+			// ! Check health of node then 
+			response->set_node_id(node_id);
+			return Status(grpc::StatusCode::NOT_FOUND, "No nodes available");
+		}
+		Status RemoveNode(ServerContext* context, const NodeInfo* request, RegisterAck* response) override {
+			lock_guard<mutex> lock(hr_mutex);
+			string node_id = request->node_id();
+			hash_ring.removeNode(node_id);
+			response->set_success(true);
+			response->set_message("Node removed successfully");
+			cout << "Node removed: " << node_id << endl;
+			return Status::OK;
+			// ! Probably add some load balancing to make it all hunky dory
+		}
+
+};
+
+void RunManagerService() {
+	string server_address("0.0.0.0:50051");
+	ManagerServiceImpl manager = ManagerServiceImpl(5);
+
+	grpc::EnableDefaultHealthCheckService(true);
+  	grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+	ServerBuilder builder;
+
+	builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+	// ! It says the next line is wrong but I'm not really sure why it would be wrong since it's like the demo?
+	builder.RegisterService(&manager);
+	unique_ptr<Server> server(builder.BuildAndStart());
+	cout << "Manager server listening on: " << server_address << endl;
+
+	server->Wait();
+}
+
+int main(int argc, char **argv) {
+	RunManagerService();
+	return 0;
+}
 
 
 // Hash Ring functions
@@ -9,30 +87,32 @@ ConsistentHashing hashRing;
 // Maybe nodes are based off id set upon initialization? like a list or 
 // map or smth to generate specific nodes
 
+
+
 // To find a node to place key-value on, pass key into get_node
-string get_node(string key) {
-	string node = hashRing.getNode(key);
-	return node;
-}
+// string get_node(string key) {
+// 	string node = hashRing.getNode(key);
+// 	return node;
+// }
 
-string add_node(string node_id) {
-	hashRing.addNode(node_id);
-}
+// string add_node(string node_id) {
+// 	hashRing.addNode(node_id);
+// }
 
-void remove_node(string node_id) {
-	hashRing.removeNode(node_id);
-}
+// void remove_node(string node_id) {
+// 	hashRing.removeNode(node_id);
+// }
 
 
 
-void GTStoreManager::init(int num_nodes) {
-	hashRing.init(num_nodes);
-	cout << "Inside GTStoreManager::init()\n";
-}
+// void GTStoreManager::init(int num_nodes) {
+// 	hashRing.init(num_nodes);
+// 	cout << "Inside GTStoreManager::init()\n";
+// }
 
-int main(int argc, char **argv) {
+// int main(int argc, char **argv) {
 
-	GTStoreManager manager;
-	manager.init(4); // Initially setting 4 nodes
+// 	GTStoreManager manager;
+// 	manager.init(4); // Initially setting 4 nodes
 	
-}
+// }
--- cs6210-project4/gtstore/src/storage.cpp
+++ cs6210-project4/gtstore/src/storage.cpp
@@ -1,7 +1,15 @@
+#include "grpcpp/security/server_credentials.h"
 #include "gtstore.hpp"
 // #include <functional>
+#include <grpcpp/server_context.h>
 #include <unordered_map>
 #include <string>
+
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+#include "proto/gtstore.grpc.pb.h"
+#include "proto/gtstore.pb.h"
 
 // ! Use unordered map to not handle node hashmap manually
 // ? Should the type be string, or vector<char> or char[]?
@@ -9,44 +17,94 @@ unordered_map<size_t, string> hashMap;
 
 // Data Storage Node functions 
 
-/** 
- *  This function puts the value into the hashmap
- *  ? Manager/whoever manages node partitions pre-hashes key
- *  @param key Key used to access hashMap.
- *  @param value Value to store in hashmap
- */
-void put(size_t key, string value) {
-	hashMap[key] = value;
-}
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
 
-/**
- *  Returns the value stored at key
- *  ? Not sure if this is how it works, do we need to make a 
- *	? copy and return? If making a copy, how do we send the
- *	? copy back because adresses spaces aren't the same?
- *  @param key Key used to access HashMap
- *  @return The string value stored in the map
- */
-string get(size_t key) {
-	return hashMap[key];
-}
+using gtstore::StorageNode;
+using gtstore::KeyValuePair;
+using gtstore::Ack;
+using gtstore::KeyRequest;
+using gtstore::ValueResponse;
 
-/**
- *  Removes the value associated with key
- *  @param key Key where value is located in hashMap
- */
-void remove(size_t key) {
-	hashMap.erase(key);
-}
+class StorageNodeImpl final : public StorageNode::Service {
+	unordered_map<string, string> kv_store;
+	mutex kv_mutex;
+	Status Put(ServerContext* context, const KeyValuePair* request, Ack* response) override {
+		lock_guard<mutex> lock(kv_mutex);
+		kv_store[request->key()] = request->value();
+		response->set_success(true);
+		response->set_message("Key-value pair stored successfully!");
+		return Status::OK;
+	}
+	Status Get(ServerContext* context, const KeyRequest* request, ValueResponse* response) override {
+		lock_guard<mutex> lock(kv_mutex);
+		auto it = kv_store.find(request->key());
+		if (it != kv_store.end()) {
+			response->set_found(true);
+			response->set_value(it->second);
+		} else {
+			response->set_found(false);
+		}
+		return Status::OK;
+	}
+};
 
-void GTStoreStorage::init(int id) {
+void RunStorageNode(int port) {
+	string server_address("0.0.0.0:" + to_string(port));
+	StorageNodeImpl node;
+	ServerBuilder builder;
 	
-	cout << "Inside GTStoreStorage::init() for node " << id << "\n";
+	grpc::EnableDefaultHealthCheckService(true);
+  	grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+	builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+
+	//! I don't know why it doesn't like the following line of code
+	builder.RegisterService(&node);
+	unique_ptr<Server> server(builder.BuildAndStart());
+	cout << "Server listening on " << server_address << endl;
+
+	server->Wait();
 }
 
 int main(int argc, char **argv) {
-
-	GTStoreStorage storage;
-	storage.init(1);
-	
+	RunStorageNode(5);
+	return 0;
 }
+
+
+// /** 
+//  *  This function puts the value into the hashmap
+//  *  ? Manager/whoever manages node partitions pre-hashes key
+//  *  @param key Key used to access hashMap.
+//  *  @param value Value to store in hashmap
+//  */
+// void put(size_t key, string value) {
+// 	hashMap[key] = value;
+// }
+
+// /**
+//  *  Returns the value stored at key
+//  *  ? Not sure if this is how it works, do we need to make a 
+//  *	? copy and return? If making a copy, how do we send the
+//  *	? copy back because addresses spaces aren't the same?
+//  *  @param key Key used to access HashMap
+//  *  @return The string value stored in the map
+//  */
+// string get(size_t key) {
+// 	return hashMap[key];
+// }
+
+// /**
+//  *  Removes the value associated with key
+//  *  @param key Key where value is located in hashMap
+//  */
+// void remove(size_t key) {
+// 	hashMap.erase(key);
+// }
+
+// void GTStoreStorage::init(int id) {
+	
+// 	cout << "Inside GTStoreStorage::init() for node " << id << "\n";
+// }
